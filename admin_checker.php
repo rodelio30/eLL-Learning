192ddcd77b9af5dd2b4b2d665aeb5f36ec145b7f
Rodelio B. Domingo Jr <rodeliobdomingojr@gmail.com>
2022-06-03 10:57:05 +0800

    change the database name

--- admin_checker.php
+++ admin_checker.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 date_default_timezone_set("Asia/Manila");
 session_start();
-if(!isset($_SESSION['logged'])){
-  header("location: public.php");
+if (!isset($_SESSION['logged'])) {
+  header("location: public/index.php");
 }
-include ('include/connect.php');
-$id=$_SESSION['id'];
+include('include/connect.php');
+$id = $_SESSION['id'];
 
-$query=mysqli_query($conn,"select id,type from users where id='$id'")or die ("query 1 incorrect.......");
-list($type)=mysqli_fetch_array($query);
+$query = mysqli_query($conn, "select id,type from users where id='$id'") or die("query 1 incorrect.......");
+list($type) = mysqli_fetch_array($query);
 
-if($type=='student'){
+if ($type == 'student') {
   header("location: student.php");
 }
