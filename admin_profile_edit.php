3a2939813022470009374f545136303b9793d173
Rodelio B. Domingo Jr <rodeliobdomingojr@gmail.com>
2022-06-22 15:25:56 +0800

    arranged the profile page for admin

---
+++ admin_profile_edit.php
@@ -0,0 +1,146 @@
+<?php
+include 'admin_checker.php';
+
+if (isset($_POST['update'])) {
+  $id        = $_POST['id'];
+  $firstname = $_POST['firstname'];
+  $lastname  = $_POST['lastname'];
+  $email     = $_POST['email'];
+  $password  = $_POST['password'];
+  $status    = $_POST['status'];
+
+  mysqli_query($conn, "update admin set firstname = '$firstname', lastname = '$lastname', email = '$email', password = '$password', status = '$status' where id = '$id'") or die("Query 4 is incorrect....");
+  // mysqli_query($conn, "update users set firstname = '$firstname', lastname = '$lastname', email = '$email', password = '$password', type = '$type' where id = '$id'") or die("Query 4 is incorrect....");
+  echo '<script type="text/javascript"> alert("User ' . $firstname . ' updated!.")</script>';
+  header('Refresh: 0; url=index.php');
+}
+
+$user_id = $_GET['ID'];
+
+$result = mysqli_query($conn, "SELECT * FROM admin WHERE user_id='$user_id'");
+while ($res   = mysqli_fetch_array($result)) {
+  $firstname = $res['firstname'];
+  $lastname  = $res['lastname'];
+  $email     = $res['email'];
+  $password  = $res['password'];
+  $status    = $res['status'];
+}
+
+$sel_active  = "";
+$sel_archive = "";
+
+if ($status == "active") {
+  $sel_active  = "selected";
+} else if ($status == "archive") {
+  $sel_archive = "selected";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include 'admin_header.php';
+?>
+
+<body>
+  <div class="wrapper">
+    <nav id="sidebar" class="sidebar js-sidebar">
+      <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.php">
+          <img src="img/icons/clsu-logo.png" alt="clsu-logo" class='mt-1 archive_photo_size'>
+        </a>
+
+        <ul class="sidebar-nav">
+          <?php
+          $nav_active = 'profile';
+          include 'admin_nav.php';
+          ?>
+        </ul>
+
+      </div>
+    </nav>
+
+    <div class="main">
+      <?php include 'admin_main_nav.php'; ?>
+
+      <main class="content">
+        <div class="container-fluid p-0">
+          <h1 class="h3 mb-3"><strong><a href="index.php" class="dash-item">Dashboard</a> / <a href="pages-profile.php"
+                class="dash-item">Admin Profile </a>/ Edit My
+              Account</strong>
+          </h1>
+          <div class="row">
+            <div class="col-12 col-lg-8 col-xxl-12 d-flex">
+              <div class="card flex-fill">
+                <div class="card-header">
+                  <h5 class="card-title mb-0">User Form</h5>
+                </div>
+                <div class="card-body">
+                  <form method="post">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Firstname</label>
+                      <input type="text" class="form-control" id="firstname" name="firstname"
+                        value="<?php echo $firstname ?>" placeholder="Enter email">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Lastname</label>
+                      <input type="text" class="form-control" id="lastname" name="lastname"
+                        value="<?php echo $lastname ?>" placeholder="Enter email">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="email" name="email" value="<?php echo $email ?>"
+                        placeholder="Enter email">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                      <label>User Status</label>
+                      <select class="form-control" id="type" value="<?php echo $status ?>" name="status">
+                        <option value="active" <?php echo $sel_active ?>>Active</option>
+                        <option value="archive" <?php echo $sel_archive ?>>Inactive</option>
+                      </select>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" class="form-control" id="password" name="password"
+                        value="<?php echo $password ?>" placeholder="Password">
+                    </div>
+                    <br>
+                    <input type="checkbox" onclick="myFunction()"> Show the Password
+                    <br>
+                    <br>
+                    <div class="form-group">
+                      <input type="hidden" name="id" value="<?php echo $_GET['ID']; ?>">
+                      <button type="submit" class="btn btn-success" name="update">Update</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </main>
+
+      <?php include 'admin_footer.php'; ?>
+    </div>
+  </div>
+
+  <script src="js/app.js"></script>
+  <script>
+  function myFunction() {
+    var pw_ele = document.getElementById("password");
+    if (pw_ele.type === "password") {
+      pw_ele.type = "text";
+    } else {
+      pw_ele.type = "password";
+    }
+  }
+  </script>
+</body>
+
+</html>
--- pages-profile.php
+++ pages-profile.php
@@ -2,26 +2,26 @@
 include 'admin_checker.php';
 
 $user_id = $id;
-$query = mysqli_query($conn, "select type from users where id='$id'") or die("query 1 incorrect.......");
-list($type) = mysqli_fetch_array($query);
 
-if ($type == 'faculty') {
-}
-if ($type == 'student') {
-}
 // echo "<script>console.log('High this is your id no: " . $id . "');</script>";
-// $result = mysqli_query($conn, "SELECT * FROM users WHERE Vfaculty_id='$id'");
-// while ($res   = mysqli_fetch_array($result)) {
-//   $user_id       = $res['id'];
-//   $firstname     = $res['firstname'];
-//   $lastname      = $res['lastname'];
-//   $course        = $res['course'];
-//   $description   = $res['description'];
-//   $email         = $res['email'];
-//   $date_created  = $res['date_created'];
-//   $date_modified = $res['date_modified'];
-//   $status        = $res['status'];
-// }
+
+$result = mysqli_query($conn, "SELECT * FROM admin WHERE user_id='$user_id'");
+while ($res   = mysqli_fetch_array($result)) {
+  $user_id       = $res['user_id'];
+  $firstname     = $res['firstname'];
+  $lastname      = $res['lastname'];
+  $email         = $res['email'];
+  $password      = $res['password'];
+  $status        = $res['status'];
+  $date_created  = $res['date_created'];
+  $date_modified = $res['date_modified'];
+}
+
+$result_history = mysqli_query($conn, "SELECT * FROM transaction_log WHERE user_id = '$user_id'");
+while ($res = mysqli_fetch_array($result_history)) {
+  $log_time = $res['log_time'];
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,169 +53,114 @@ include 'admin_header.php';
 
       <main class="content">
         <div class="container-fluid p-0">
-          <h1 class="h3 mb-3">Document </h1>
+          <h1 class="h3 mb-3"><strong><a href="index.php" class="dash-item">Dashboard</a> / Admin Profile</strong>
+          </h1>
+          <div class="page-content">
 
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <div class="emp-profile">
-                  <form method="post">
-                    <div class="row">
-                      <div class="col-md-4">
-                        <div class="profile-img">
-                          <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog"
-                            alt="" />
-                          <div class="file btn btn-lg btn-primary">
-                            Change Photo
-                            <input type="file" name="file" />
+            <div class="row">
+              <div class="col-12 col-lg-8 col-xxl-12 d-flex">
+                <div class="card-view flex-fill">
+                  <div class="main-body">
+                    <div class="row gutters-sm">
+                      <div class="col-md-3 mb-3 mt-4 border-end">
+                        <div class="d-flex flex-column align-items-center text-center">
+                          <img src="img/icons/user.png" alt="Admin" class="rounded-circle" width="150">
+                          <div class="mt-3">
+                            <h4><?php echo $firstname . " " . $lastname ?></h4>
+                            <p class="text-secondary mb-1"><?php echo $status ?></p>
                           </div>
                         </div>
                       </div>
-                      <div class="col-md-6">
-                        <div class="profile-head">
-                          <h5>
-                            Kshiti Ghelani
-                          </h5>
-                          <h6>
-                            Web Developer and Designer
-                          </h6>
-                          <p class="proile-rating">RANKINGS : <span>8/10</span></p>
-                          <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item">
-                              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
-                                aria-controls="home" aria-selected="true">About</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
-                                aria-controls="profile" aria-selected="false">Timeline</a>
-                            </li>
-                          </ul>
+                      <div class="col-md-9">
+                        <div class="card me-4 mt-4">
+                          <div class="card-body">
+                            <div class="row">
+                              <div class="col-sm-3">
+                                <h6 class="mb-0 flatpickr-weekwrapper"><strong>Full Name</strong></h6>
+                              </div>
+                              <div class="col-sm-9 text-secondary">
+                                <div class="flatpickr-weekwrapper">
+                                  <?php echo $firstname . " " . $lastname ?>
+                                </div>
+                              </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                              <div class="col-sm-3">
+                                <h6 class="mb-0 flatpickr-weekwrapper"><strong>Email</strong></h6>
+                              </div>
+                              <div class="col-sm-9 text-secondary">
+                                <div class="flatpickr-weekwrapper">
+                                  <?php echo $email ?>
+                                </div>
+                              </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                              <div class="col-sm-3">
+                                <h6 class="mb-0 flatpickr-weekwrapper"><strong>Status</strong></h6>
+                              </div>
+                              <div class="col-sm-9 text-secondary">
+                                <div class="flatpickr-weekwrapper">
+                                  <?php echo $status ?>
+                                </div>
+                              </div>
+                            </div>
+                            <hr>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-2">
-                        <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile" />
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-4">
-                        <div class="profile-work">
-                          <p>WORK LINK</p>
-                          <a href="">Website Link</a><br />
-                          <a href="">Bootsnipp Profile</a><br />
-                          <a href="">Bootply Profile</a>
-                          <p>SKILLS</p>
-                          <a href="">Web Designer</a><br />
-                          <a href="">Web Developer</a><br />
-                          <a href="">WordPress</a><br />
-                          <a href="">WooCommerce</a><br />
-                          <a href="">PHP, .Net</a><br />
-                        </div>
-                      </div>
-                      <div class="col-md-8">
-                        <div class="tab-content profile-tab" id="myTabContent">
-                          <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>User Id</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>Kshiti123</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Name</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>Kshiti Ghelani</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Email</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>kshitighelani@gmail.com</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Phone</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>123 456 7890</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Profession</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>Web Developer and Designer</p>
+
+                        <div class="row gutters-sm">
+                          <div class="col-sm-12 mb-2">
+                            <div class="card h-100 mb-2 me-4">
+                              <div class="card-body">
+                                <h5 class="d-flex align-items-center mb-3"><b>About History Log</b></h5>
+                                <div class="row">
+                                  <div class="col-sm-3">
+                                    <p class="flatpickr-weekwrapper">Date Created</p>
+                                  </div>
+                                  <div class="col-sm-7">
+                                    <p class="flatpickr-weekwrapper"><?php echo $date_created ?></p>
+                                  </div>
+                                </div>
+                                <div class="row">
+                                  <div class="col-sm-3">
+                                    <p class="flatpickr-weekwrapper">Date Modified</p>
+                                  </div>
+                                  <div class="col-sm-7">
+                                    <p class="flatpickr-weekwrapper"><?php echo $date_modified ?></p>
+                                  </div>
+                                </div>
+                                <div class="row">
+                                  <div class="col-sm-3">
+                                    <p class="flatpickr-weekwrapper">Latest Signed in</p>
+                                  </div>
+                                  <div class="col-sm-9">
+                                    <p class="flatpickr-weekwrapper"><?php echo $log_time ?></p>
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
-                          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Experience</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>Expert</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Hourly Rate</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>10$/hr</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Total Projects</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>230</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>English Level</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>Expert</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <label>Availability</label>
-                              </div>
-                              <div class="col-md-6">
-                                <p>6 months</p>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-md-12">
-                                <label>Your Bio</label><br />
-                                <p>Your detail description</p>
-                              </div>
-                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-sm-12">
+                            <a class="btn btn-info mb-2" <?php echo "href=\"admin_profile_edit.php?ID=$user_id\" " ?>
+                              style="float: left;">Edit</a>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </form>
-                </div>
-                <!-- End of content -->
-                <div class="card-body">
+
+                  </div>
+                  <!-- </div> -->
+                  <!-- </div> -->
                 </div>
               </div>
             </div>
+            <!-- end of row -->
           </div>
-
         </div>
       </main>
 
