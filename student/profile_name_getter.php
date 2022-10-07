13c8d21fdf34991d8991ff09ff53aa3ba123dc1b
Rodelio B. Domingo Jr <rodeliobdomingojr@gmail.com>
2022-10-07 10:11:02 +0800

    Student Can update their own information

--- student/profile_name_getter.php
+++ student/profile_name_getter.php
@@ -1,12 +1,19 @@
 <?php
 $id = $_SESSION['id'];
 
-$query = mysqli_query($conn, "select student_id, firstname, lastname, email from student where user_id='$id'") or die("query 1 incorrect.......");
-list($student_id, $firstname, $lastname, $email) = mysqli_fetch_array($query);
-$student_id = $student_id;
-$student_firstname = $firstname;
-$student_lastname = $lastname;
-$student_email = $email;
+$query = mysqli_query($conn, "select student_id, img, firstname, middle_initial, lastname, description, email, password, student_course, student_year, student_section from student where user_id='$id'") or die("query 1 incorrect.......");
+list($student_id, $img, $firstname, $middle_initial, $lastname, $description, $email, $password, $student_course, $student_year, $student_section) = mysqli_fetch_array($query);
+$student_id          = $student_id;
+$student_img         = $img;
+$student_firstname   = $firstname;
+$student_mi          = $middle_initial;
+$student_lastname    = $lastname;
+$student_description = $description;
+$student_email       = $email;
+$student_password    = $password;
+$student_course      = $student_course;
+$student_year        = $student_year;
+$student_section     = $student_section;
 
 $fname =  substr($firstname, 0, 1);
 $lname =  substr($lastname, 0, 1);
