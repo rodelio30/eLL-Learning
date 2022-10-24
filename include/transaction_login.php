cf89f0a0ba8a2cf9973bacfdbc188f58152d9673
Rodelio B. Domingo Jr <rodeliobdomingojr@gmail.com>
2022-10-24 13:35:04 +0800

    system know the user gender when log in

--- include/transaction_login.php
+++ include/transaction_login.php
@@ -3,8 +3,20 @@ include 'connect.php';
 $id = $_SESSION['id'];
 
 
-$query = mysqli_query($conn, "select id from users where id='$id'") or die("query 1 incorrect.......");
-list($user_id) = mysqli_fetch_array($query);
+$query = mysqli_query($conn, "select id, type from users where id='$id'") or die("query 1 incorrect.......");
+list($user_id, $type_logged) = mysqli_fetch_array($query);
+
+// query to add gender for the user 
+if ($type_logged == 'faculty') {
+  $query_gender_faculty = mysqli_query($conn, "select gender from faculty where user_id='$user_id'") or die("query 1 incorrect.......");
+  list($gender_login) = mysqli_fetch_array($query_gender_faculty);
+} else if ($type_logged == 'student') {
+  $query_gender_student = mysqli_query($conn, "select gender from student where user_id='$user_id'") or die("query 1 incorrect.......");
+  list($gender_login) = mysqli_fetch_array($query_gender_student);
+} else if ($type_logged == 'admin') {
+  $query_gender_admin = mysqli_query($conn, "select gender from admin where user_id='$user_id'") or die("query 1 incorrect.......");
+  list($gender_login) = mysqli_fetch_array($query_gender_admin);
+}
 
 $transaction_name = 'Log in';
 $date             = date("Y-m-d");
@@ -12,4 +24,4 @@ $log_time         = date("Y-m-d h:i:s");
 $action           = 'Execute';
 $status           = 'Successful';
 
-mysqli_query($conn, "insert into transaction_log(user_id, transaction_name, date, log_time, action, status) values('$user_id','$transaction_name','$date','$log_time','$action','$status')")  or die("Query 3 is incorrect.....");
+mysqli_query($conn, "insert into transaction_log(user_id, transaction_name, gender, user_type, date, log_time, action, status) values('$user_id','$transaction_name','$gender_login','$type_logged','$date','$log_time','$action','$status')")  or die("Query 3 is incorrect.....");
